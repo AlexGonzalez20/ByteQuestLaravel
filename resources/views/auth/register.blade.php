1581568627a179455dc8223451d709e229699840
Andrés Miranda <andresfelipemiranda19@gmail.com>
2025-05-21 23:02:07 -0500

    Diseño de register actualizado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,36 +1,50 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Registro - ByteQuest</title>
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    @vite('resources/css/register.css')
+
+
 </head>
+
 <body>
-    <h2>Registrarse</h2>
-
-    @if ($errors->any())
-        <div style="color:red;">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="container">
+        <div class="header">
+            <header>
+                <h2>Registrarse</h2>
+            </header>
         </div>
-    @endif
+        <div class="content-table">
+            @if ($errors->any())
+                <div style="color:red;">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="formulario">
 
-    <form method="POST" action="/register">
-        @csrf
-        <label for="correo">Correo:</label>
-        <input type="email" name="correo" required><br>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" required><br>
-
-        <label for="password_confirmation">Confirmar Contraseña:</label>
-        <input type="password" name="password_confirmation" required><br>
-
-        <button type="submit">Registrarse</button>
-    </form>
-
-    <br>
-    <a href="/login">¿Ya tienes cuenta? Inicia sesión</a>
+            <div class="card-form">
+                <form method="POST" action="/register">
+                    @csrf
+                    <label for="correo">Correo:</label>
+                    <input type="email" name="correo" placeholder="Correo" required><br>
+                    <label for="password">Contraseña:</label>
+                    <input type="password" name="password" placeholder="Contraseña" required><br>
+                    <label for="password_confirmation">Confirmar Contraseña:</label>
+                    <input type="password" name="password_confirmation" required placeholder="Confirmar Contraseña"><br>
+                    <button type="submit">Registrarse</button>
+                </form>
+                <br>
+                <a href="/login">¿Ya tienes cuenta? Inicia sesión</a>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
